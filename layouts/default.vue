042295f27b2a11b42075567f493951d2e3f192b0
CristopherMartins <cmartins@bonadmin.com>
2023-02-28 18:01:22 -0300

    template default structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
+    <div class="body__container">
         <TheHeader />
-        <slot />
+        <main>
+            <slot />
+        </main>
         <TheFooter />
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .body__container{
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        main{
+            flex-grow: 1;
+        }
+    }
+</style>
