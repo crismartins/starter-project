e7b5d5b90a893ff7a51ce6bf1e5fb9b20401ea58
CristopherMartins <cmartins@bonadmin.com>
2023-03-01 17:42:22 -0300

    update icon component

--- components/AppIcon.vue
+++ components/AppIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <i class="icon__wrapper">
-	    <Icon :name="props.iconName" />
+	    <Icon :name="props.iconName" :class="`icon-size__${props.iconSize}`" />
     </i>
 </template>
 
@@ -9,12 +9,30 @@
         iconName: {
             type: String,
             default: "mdi:emoticon-cool-outline"
-        }
+        },
+        iconSize: {
+            type: String,
+            default: "default"
+        },
     })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     .icon__wrapper{
         display: initial;
+        .icon-size{
+            &__default{
+                font-size: $size_16px;
+            }
+            &__small{
+                font-size: $size_small;
+            }
+            &__medium{
+                font-size: $size_medium;
+            }
+            &__large{
+                font-size: $size_large;
+            }
+        }
     }
 </style>
