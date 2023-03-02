0e6ef9244d94344f241c3148e2cf85f61c6c092a
CristopherMartins <cmartins@bonadmin.com>
2023-03-02 18:15:26 -0300

    add login layout

---
+++ layouts/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="body__container">
+        <TheHeader />
+        <main>
+            <slot />
+        </main>
+        <TheFooter />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .body__container{
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        main{
+            flex-grow: 1;
+        }
+    }
+</style>
